7ff2efcc8af83fee79687220e286c9a8f782675d
SurajPatel04 <surajpatel9390@gmail.com>
2025-04-08 22:42:59 +0530

    fix

--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -1,23 +1,8 @@
 function generatToken(text) {
-  const tokens = [];
-  let hold = 0;
+  let tokens = [];
 
   for (let i = 0; i < text.length; i++) {
-    const char = text[i];
-
-    if (/[^a-zA-Z0-9]/.test(char)) {
-      if (hold !== 0) {
-        tokens.push(hold);
-        hold = 0;
-      }
-      tokens.push(text.charCodeAt(i));
-    } else {
-      hold += text.charCodeAt(i);
-    }
-  }
-
-  if (hold !== 0) {
-    tokens.push(hold);
+    tokens.push(text.charCodeAt(i));
   }
 
   return tokens;
